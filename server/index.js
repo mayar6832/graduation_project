a2c75df48612683f7f0542aa7102317212dabf3a
mayar6832 <76603384+mayar6832@users.noreply.github.com>
2023-05-23 14:34:02 +0300

    test

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
-import morgan from "morgan";
+
 import path from "path";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
