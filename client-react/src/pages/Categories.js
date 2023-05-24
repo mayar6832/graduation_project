cee5cda51243833486d53c2d737e86aa645f400c
yarZakaria <mayar123yara>
2023-05-24 16:52:35 +0200

    Ctegories

---
+++ client-react/src/pages/Categories.js
@@ -0,0 +1,50 @@
+import SearchCard from './../components/SearchCard';
+import PriceFilter from './../components/PriceFilter';
+import NavBar from './../components/NavBar';
+import Footer from './../components/Footer';
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useSearchParams } from "react-router-dom";
+import { useState } from 'react';
+import { Stack } from '@mui/material';
+
+function Categories() {
+    const { categoryName } = useParams();
+    console.log(categoryName)
+    const [searchParams] = useSearchParams();
+    const [items, setItems] = useState([]);
+    const [totalPages, setTotalPages] = useState();
+    const [numProduct, setNumProduct] = useState([]);
+    useEffect(() => {
+        const getProduct = () => {
+            axios.post(`http://localhost:5500/product/search_category?page=${searchParams.get('page')}`, {
+                "search":categoryName , "maxPrice": searchParams.get("priceto"), "minPrice": searchParams.get("pricefrom")
+            })
+                .then(res => {
+                    console.log(res.data.data)
+                    setTotalPages(res.data.total_pages)
+                    setNumProduct(res.data.length)
+                    setItems(res.data.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+        getProduct();
+    }, [categoryName,searchParams]);
+    return (
+        <>
+            <NavBar />
+            <Stack direction="row" spacing={2}>
+                <PriceFilter />
+                <SearchCard items={items} pages={totalPages} length={numProduct}/>
+            </Stack>
+            <Footer />
+        </>
+
+
+    );
+}
+
+export default Categories;
