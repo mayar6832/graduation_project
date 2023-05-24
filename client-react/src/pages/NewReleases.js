ac34c7586f8dbf3039c41dd31376856790dae199
yarZakaria <mayar123yara>
2023-05-24 16:51:45 +0200

    NewRealese frontend

---
+++ client-react/src/pages/NewReleases.js
@@ -0,0 +1,17 @@
+import NavBar from './../components/NavBar';
+import Footer from './../components/Footer';
+import SearchCard from './../components/SearchCard';
+import PriceFilter from './../components/PriceFilter';
+
+function NewReleases() {
+    return (
+        <>
+            <NavBar />
+            <Footer />
+        </>
+
+
+    );
+}
+
+export default NewReleases;
