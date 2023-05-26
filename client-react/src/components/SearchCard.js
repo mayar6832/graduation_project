6ea943f122e9bfaf449797a510c9c60303205aee
yarZakaria <mayar123yara>
2023-05-26 13:16:18 +0200

    fix styling

--- client-react/src/components/SearchCard.js
+++ client-react/src/components/SearchCard.js
@@ -45,7 +45,6 @@ function SearchCard({ items, pages, length }) {
                     borderLeft: "1px outset #BDBDBD",
                     borderTop: "1px outset #BDBDBD",
                     borderBottom: "1px outset #BDBDBD",
-                    width:"100%"
                 }}
             >
                 <Typography
